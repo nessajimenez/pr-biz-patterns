652c648c0129d3d4fdb0efdf291f241f8226aaa1
nessajimenez <168484714+nessajimenez@users.noreply.github.com>
2024-07-19 15:53:02 -0400

    updated code to clean and functions

--- Biz_Set_Functions.py
+++ Biz_Set_Functions.py
@@ -9,22 +9,31 @@ import matplotlib.pyplot as plt
 
 # these are the functions used to clean our data set
 
-def made_df(file_name):
+def made_df(file_name, year):
     try:
-        content = pd.read_csv(file_name)
-        return content
+        df = pd.read_csv(file_name)
+        df.columns = df.columns.str.strip().str.lower()
+        df['year']= year
+        return df
     
     except FileNotFoundError:
         print("File not found")
         
-def lower_columns(data_frame):
-    data_frame.columns = data_frame.columns.str.lower()
-    return data_frame.columns
-
-def add_year(df,year):
-    df['year'] = year
-    return df['year']
-
+        
+def combined_df(file_names, year):
+    dataframes = []
+    for file_name in file_names:
+        try:
+            df = pd.read_csv(file_name)
+            df.columns = df.columns.str.strip().str.lower()
+            df['year'] = year
+            dataframes.append(df)
+        except FileNotFoundError:
+            print(f"File not found: {file_name}")
+    
+    combined_df = pd.concat(dataframes, ignore_index=True)
+    return combined_df
+        
 
 def replace_naics(naics_code):
     # Dictionary mapping NAICS codes to industry names
@@ -34,12 +43,11 @@ def replace_naics(naics_code):
     "72": "72","81": "81","92": "92"
 }
     
-    
     industry_code = naics_code[:2]  # Extract first two digits of NAICS code
-    return naics_dict.get(industry_code, "UI")
+    return naics_dict.get(industry_code, "00")
 
 
-def replace_town_with_region(town):
+def replace_town_with_region(df,column):
     
     puerto_rico_regions = {
     "1": "Mountain","3": "Western","5": "Western","7": "Metro Area","9": "Mountain","11": "Western","13": "Northern",
@@ -55,11 +63,32 @@ def replace_town_with_region(town):
     "143": "Northern","145": "Northern","147": "Eastern","149": "Southern","151": "Eastern","153": "Western",
 }
 
-    # Check if the town exists in the dictionary
-    if town in puerto_rico_regions:
-        # Replace the town with its corresponding region
-        return puerto_rico_regions[town]
-    else:
-        # Return the original town name if it doesn't exist in the dictionary
-        return town
+    # Replace the town with its corresponding region
+    return df[column].map(lambda x: puerto_rico_regions.get(str(x), x))
+
+
+def replace_code_with_town(df, column):
+
+    pr_towns = {
+    '001': 'Adjuntas', '003': 'Aguada', '005': 'Aguadilla', '007': 'Aguas Buenas', '009': 'Aibonito',
+    '011': 'Anasco', '013': 'Arecibo', '015': 'Arroyo', '017': 'Barceloneta', '019': 'Barranquitas',
+    '021': 'Bayamon', '023': 'Cabo Rojo', '025': 'Caguas', '027': 'Camuy', '029': 'Canovanas',
+    '031': 'Carolina', '033': 'Catano', '035': 'Cayey', '037': 'Ceiba', '039': 'Ciales',
+    '041': 'Cidra', '043': 'Coamo', '045': 'Comerio', '047': 'Corozal', '049': 'Culebra',
+    '051': 'Dorado', '053': 'Fajardo', '054': 'Florida', '055': 'Guanica', '057': 'Guayama',
+    '059': 'Guayanilla', '061': 'Guaynabo', '063': 'Gurabo', '065': 'Hatillo', '067': 'Hormigueros',
+    '069': 'Humacao', '071': 'Isabela', '073': 'Jayuya', '075': 'Juana Diaz', '077': 'Juncos',
+    '079': 'Lajas', '081': 'Lares', '083': 'Las Marias', '085': 'Las Piedras', '087': 'Loiza',
+    '089': 'Luquillo', '091': 'Manati', '093': 'Maricao', '095': 'Maunabo', '097': 'Mayaguez',
+    '099': 'Moca', '101': 'Morovis', '103': 'Naguabo', '105': 'Naranjito', '107': 'Orocovis',
+    '109': 'Patillas', '111': 'Penuelas', '113': 'Ponce', '115': 'Quebradillas', '117': 'Rincon',
+    '119': 'Rio Grande', '121': 'Sabana Grande', '123': 'Salinas', '125': 'San German', '127': 'San Juan',
+    '129': 'San Lorenzo', '131': 'San Sebastian', '133': 'Santa Isabel', '135': 'Toa Alta', '137': 'Toa Baja',
+    '139': 'Trujillo Alto', '141': 'Utuado', '143': 'Vega Alta', '145': 'Vega Baja', '147': 'Vieques',
+    '149': 'Villaba', '151': 'Yabucoa', '153': 'Yauco', '999': 'Statewide'
+        }
+
+    # Replace the code with its corresponding town
+    return df[column].map(lambda x: pr_towns.get(str(x), x))
+
 
